5be562ef63c3e904584bef5c9b7ebbc21824bb65
Benjamin Purcell <>
2024-07-20 06:52:30 -0600

    Word Search, slight optimization

--- lib/word_search.h
+++ lib/word_search.h
@@ -4,6 +4,50 @@
 #include <string>
 #include <vector>
 
+class SolutionSlow {
+  public:
+    bool findWord(std::vector<std::vector<char>> &board,
+                  decltype(board.size()) i, decltype(board[i].size()) j,
+                  std::string_view s) {
+        if (board[i][j] != s.front()) return false;
+        if (s.size() == 1) return true;
+        auto tmp = board[i][j];
+        board[i][j] = '\0';
+        auto found = [this, &i, &j, &board, &s]() {
+            if (i > 0 && findWord(board, i - 1, j, s.substr(1))) return true;
+            if (j > 0 && findWord(board, i, j - 1, s.substr(1))) return true;
+            if (i < board.size() - 1 && findWord(board, i + 1, j, s.substr(1)))
+                return true;
+            if (j < board.size() - 1 && findWord(board, i, j + 1, s.substr(1)))
+                return true;
+            return false;
+        }();
+        board[i][j] = tmp;
+        return found;
+    }
+
+    bool findWord(std::vector<std::vector<char>> &board,
+                  const std::string &word) {
+        if (word.empty()) return false;
+        for (decltype(board.size()) i = 0; i < board.size(); ++i) {
+            for (decltype(board[i].size()) j = 0; j < board[i].size(); ++j) {
+                if (findWord(board, i, j, std::string_view(word))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    std::vector<std::string> findWords(std::vector<std::vector<char>> &board,
+                                       std::vector<std::string> &words) {
+        std::vector<std::string> acc;
+        for (auto w : words)
+            if (findWord(board, w)) acc.push_back(w);
+        return acc;
+    }
+};
+
 class Trie {
   public:
     Trie() : parent(nullptr), val('\0'), term(false), found(false) {}
@@ -61,10 +105,6 @@ class Solution {
             if (c != board[i][j]) continue;
             if (n->term && !n->found) {
                 found.push_back(n->getWord());
-                if (n->children.empty()) {
-                    n->parent->erase(c);
-                    continue;
-                }
                 n->found = true;
             }
             auto saved = board[i][j];
@@ -74,6 +114,7 @@ class Solution {
             if (i < board.size() - 1) findWords(board, i + 1, j, n, found);
             if (j < board[i].size() - 1) findWords(board, i, j + 1, n, found);
             board[i][j] = saved;
+            if (n->found && n->children.empty()) n->parent->erase(c);
         }
     }
 
@@ -87,48 +128,4 @@ class Solution {
                 findWords(board, i, j, &trie, found);
         return found;
     };
-};
-
-class SolutionSlow {
-  public:
-    bool findWord(std::vector<std::vector<char>> &board,
-                  decltype(board.size()) i, decltype(board[i].size()) j,
-                  std::string_view s) {
-        if (board[i][j] != s.front()) return false;
-        if (s.size() == 1) return true;
-        auto tmp = board[i][j];
-        board[i][j] = '\0';
-        auto found = [this, &i, &j, &board, &s]() {
-            if (i > 0 && findWord(board, i - 1, j, s.substr(1))) return true;
-            if (j > 0 && findWord(board, i, j - 1, s.substr(1))) return true;
-            if (i < board.size() - 1 && findWord(board, i + 1, j, s.substr(1)))
-                return true;
-            if (j < board.size() - 1 && findWord(board, i, j + 1, s.substr(1)))
-                return true;
-            return false;
-        }();
-        board[i][j] = tmp;
-        return found;
-    }
-
-    bool findWord(std::vector<std::vector<char>> &board,
-                  const std::string &word) {
-        if (word.empty()) return false;
-        for (decltype(board.size()) i = 0; i < board.size(); ++i) {
-            for (decltype(board[i].size()) j = 0; j < board[i].size(); ++j) {
-                if (findWord(board, i, j, std::string_view(word))) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    std::vector<std::string> findWords(std::vector<std::vector<char>> &board,
-                                       std::vector<std::string> &words) {
-        std::vector<std::string> acc;
-        for (auto w : words)
-            if (findWord(board, w)) acc.push_back(w);
-        return acc;
-    }
 };
