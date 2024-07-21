033225b40184005985fa23079fa71c0d1af51c3f
Benjamin Purcell <>
2024-07-21 14:41:24 -0600

    Merge K Sorted Lists is faster with priority_queue

--- lib/merge_sorted_lists.h
+++ lib/merge_sorted_lists.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <queue>
 #include <vector>
 
 struct ListNode {
@@ -11,7 +12,7 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-class Solution {
+class SolutionSlow {
   public:
     ListNode *mergeKLists(std::vector<ListNode *> &lists) {
         ListNode *l = nullptr;
@@ -30,6 +31,30 @@ class Solution {
             push_back(*mi);
             if (next == nullptr) lists.erase(mi);
             else *mi = next;
+        }
+        return l;
+    }
+};
+
+class Solution {
+  public:
+    ListNode *mergeKLists(std::vector<ListNode *> &lists) {
+        ListNode *l = nullptr;
+        ListNode **tail = &l;
+        auto push_back = [&tail](ListNode *n) {
+            *tail = n;
+            tail = &n->next;
+            n->next = nullptr;
+        };
+        std::erase_if(lists, [](ListNode *n) { return n == nullptr; });
+        auto cmp = [](ListNode *n1, ListNode *n2) { return n1->val > n2->val; };
+        std::priority_queue<ListNode *, std::vector<ListNode *>, decltype(cmp)>
+            pq(cmp, lists);
+        while (!pq.empty()) {
+            auto n = pq.top();
+            pq.pop();
+            if (n->next != nullptr) pq.push(n->next);
+            push_back(n);
         }
         return l;
     }
